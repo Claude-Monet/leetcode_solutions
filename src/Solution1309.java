2a4fb96362d0497fa7858ab45b56bcbf2905a7ad
Jianfeng Guo <jianfeng.guo.0309@gmail.com>
2020-03-18 21:25:27 -0700

    Improve solution_1309

--- src/Solution1309.java
+++ src/Solution1309.java
@@ -1,15 +1,16 @@
 public class Solution1309 {
+    // traverse from right to left, so don't have to check (i + 2)
     public String freqAlphabets(String s) {
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < s.length(); i++) {
+        for(int i = s.length() - 1; i >= 0; i--) {
             char c = s.charAt(i);
-            if (c >= '1' && c <= '9' && i + 2 < s.length() && s.charAt(i + 2) == '#') {
-                sb.append((char)(Integer.parseInt(s.substring(i, i + 2)) - 10 + 'j'));
-                i += 2;
+            if (c == '#') {
+                sb.append((char)((s.charAt(i - 1) - '0') + 10 * (s.charAt(i - 2) - '0') - 10 + 'j'));
+                i -= 2;
             } else {
-                sb.append((char)(c - '0' + 'a'));
+                sb.append((char)(c - '1' + 'a'));
             }
         }
-        return sb.toString();
+        return sb.reverse().toString();
     }
 }
