b319e3e9f6de86b5c836f39869c334ead349fd49
Jianfeng Guo <jianfeng.guo.0309@gmail.com>
2020-03-21 19:03:59 -0700

    Add  solution_857, solution_1383

--- src/Solution.java
+++ src/Solution.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] speed = new int[]{2,10,3,1,5,8}, eff = new int[]{5,4,3,9,7,2};
         System.out.println(100_001);
     }
 
