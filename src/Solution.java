943eccfda0d4972ea998f93de4e804e2795d2141
Jianfeng Guo <jianfeng.guo.0309@gmail.com>
2020-03-25 01:38:35 -0700

    Add  solution_1391, solution_1392

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,6 @@
-import java.util.*;
-
 public class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
         System.out.println(100_001);
-    }
-
-    public boolean hasValidPath(int[][] grid) {
-        Queue<int[]> q = new LinkedList<>(); q.add(new int[]{0, 0});
-        Set<String> visited = new HashSet<>(); visited.add(0 + " " + 0);
-        while (!q.isEmpty()) {
-            int[] c = q.poll();
-            int street = grid[c[0]][c[1]];
-
-        }
-        return false;
     }
 }
