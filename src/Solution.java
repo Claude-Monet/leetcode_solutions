a3d2cab05693a7a653d779b908073c25edeea02f
Jianfeng Guo <jianfeng.guo.0309@gmail.com>
2020-07-21 19:40:54 -0700

    add some temp code in Soluton.java

--- src/Solution.java
+++ src/Solution.java
@@ -2,9 +2,76 @@ import java.lang.reflect.Array;
 import java.util.*;
 
 public class Solution {
+
+    int min = Integer.MAX_VALUE;
+    public int assignBikes(int[][] workers, int[][] bikes) {
+        int m = workers.length, n = bikes.length;
+        boolean[] visited = new boolean[n];
+        assign(workers, bikes, visited, 0, 0);
+        return min;
+    }
+
+    private int dis(int[] worker, int[] bikes) {
+        return Math.abs(worker[0] - bikes[0]) + Math.abs(worker[1] - bikes[1]);
+    }
+
+    private void assign(int[][] workers, int[][] bikes, boolean[] visited, int idx, int sum) {
+        if (idx == workers.length) {
+            min = Math.min(min, sum);
+            return;
+        }
+
+        for(int i = 0; i < bikes.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                assign(workers, bikes, visited, idx + 1, sum + dis(workers[idx], bikes[i]));
+                visited[i] = false;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Solution s = new Solution();
+
     }
+
+    public int numPoints(int[][] points, int r) {
+        int l = points.length;
+        Map<String, Integer> m = new HashMap<>();
+
+        for(int i = 0; i < l; i++) {
+            int x1 = points[i][0], y1 = points[i][1];
+            for (int j = i + 1; j < l; j++) {
+                int x2 = points[j][0], y2 = points[j][1];
+                for (int k = j + 1; k < l; k++) {
+                    int x3 = points[k][0], y3 = points[k][1];
+                    int a = getA(x1, x2, x3, y1, y2, y3), b = getB(x1, x2, x3, y1, y2, y3), c = getC(x1, x2, x3, y1, y2, y3);
+                    double x = -(double)b/(2 * a), y = -(double)c/(2 * a);
+                    String key = String.format("%.2f", x) + " " + String.format("%.2f", y);
+                    m.put(key, m.getOrDefault(key, 0) + 1);
+                }
+            }
+        }
+
+        int max = 0;
+        for(Map.Entry<String, Integer> v : m.entrySet()) {
+            max = Math.max(max, v.getValue());
+        }
+        return max;
+    }
+
+    int getA(int x1, int x2, int x3, int y1, int y2, int y3) {
+        return x1 * (y2 - y3) - y1 * (x2 - x3) + x2 * y3 - x3 * y2;
+    }
+
+    int getB(int x1, int x2, int x3, int y1, int y2, int y3) {
+        return (x1 * x1 + y1 * y1) * (y3 - y2) + (x2 * x2 + y2 * y2) * (y1 - y3) + (x3 ^ x3 + y3 ^ y3) * (y2 - y1);
+    }
+
+    int getC(int x1, int x2, int x3, int y1, int y2, int y3) {
+        return (x1 * x1 + y1 * y1) * (x2 - x3) + (x2 * x2 + y2 * y2) * (x3 - x1) + (x3 ^ x3 + y3 ^ y3) * (x1 - x2);
+    }
+
 
     public int[] maxSlidingWindow(int[] nums, int k) {
         if(k < 0 || nums == null || nums.length == 0) return new int[0];
